284efd3133e009274b99dc2bfc3aae310ec7d39a
Muralikrishna Patibandla <muralikrishna@krypc.com>
2024-05-27 13:35:32 +0530

    improving styling and layout

--- src/App.js
+++ src/App.js
@@ -41,34 +41,44 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>IPFS File Manager Demo Dapp</h1>
+        <hr />
 
-        {files.map((file, index) => (
-          <div key={index}>
-            <p>{file.name}</p>
-          </div>
-        ))}
-        {files.length < 10 && <input type="file" onChange={handleAddFile} />}
-        <button onClick={handleUploadFiles} disabled={files.length === 0}>Upload to IPFS</button>
+        <div className="file-upload-section">
+        <h2>Upload Files to IPFS</h2>
+          {files.length > 0 && (
+            <>
+              <ul className="file-list">
+                {files.map((file, index) => (
+                  <li key={index}>
+                    {file.name} {hashes[index] && <span>- {hashes[index]}</span>}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
 
-        <ul>
-          {hashes.map((fileHash, index) => (
-            <li key={index}>{fileHash}</li>
-          ))}
-        </ul>
+          {files.length < 10 && <input type="file" onChange={handleAddFile} />}
+          <button onClick={handleUploadFiles} disabled={files.length === 0}>Upload to IPFS</button>
+        </div>
 
-        <input
-          type="text"
-          value={hash}
-          onChange={(e) => setHash(e.target.value)}
-          placeholder="Enter Hash"
-        />
-        <button onClick={handleGetFile}>Get File</button>
+        <hr />
 
-        {downloadUrl && (
-          <button onClick={() => window.open(downloadUrl, '_blank')}>
-            Open File
-          </button>
-        )}
+        <div className="file-retrieve-section">
+          <h2>Get File from IPFS</h2>
+          <input
+            type="text"
+            value={hash}
+            onChange={(e) => setHash(e.target.value)}
+            placeholder="Enter Hash"
+          />
+          <button onClick={handleGetFile}>Get File</button>
+
+          {downloadUrl && (
+            <button onClick={() => window.open(downloadUrl, '_blank')}>
+              Open File
+            </button>
+          )}
+        </div>
       </header>
     </div>
   );
